20a92c77615c19899d504a282998a7c0655e945d
CodeCraftsman-Jr <vasanthan22td0728@svcet.ac.in>
2025-01-31 11:18:55 +0530

    fff

--- flask_project/app.py
+++ flask_project/app.py
@@ -9,6 +9,13 @@ def home():
 @app.route('/hello/<name>')
 def hello(name):
     return f"Hello, {name}!"
+@app.route('/vasanth')
+def vasanth():
+    return f"route node arrived"
+
+@app.route('/hell/<vasanth>')
+def hell(vasanth):
+    return f"Welcome home, {vasanth}!"
 
 if __name__ == '__main__':
     app.run(debug=True)
